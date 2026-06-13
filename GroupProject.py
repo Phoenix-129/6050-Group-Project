1da9a9b0efa654b3a2b52a9e91114eee0e53bf8b
phoenixa1229-web <phoenixa1229@gmail.com>
2026-06-12 22:37:26 -0400

    Create GroupProject.py

---
+++ GroupProject.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Created on: [date here]
+@Author: [Phoenix A., Griffin B., Wyatt G., Courtney K., Kristi P]
+@Course: INF 6050
+@University: Wayne State University
+@Assignment: [Group Project]
+    
+@Python Version: 3.8x   
+@Required Modules: [any required modules]
+    
+@Description: [code description - what does this accomplish?]
+"""
+########################### 
+# GLOBAL VARIABLES
+###########################
+
+
+########################### 
+# USER-DEFINED FUNCTIONS
+###########################
+
+
+########################### 
+# SCRIPT HERE
+###########################
