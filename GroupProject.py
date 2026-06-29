0ad3e01463cde94a182c3f385ae1842667bdc1a1
phoenixa1229-web <phoenixa1229@gmail.com>
2026-06-29 13:43:06 -0400

    added two files and example to the py file

--- GroupProject.py
+++ GroupProject.py
@@ -12,6 +12,18 @@
     
 @Description: [code description - what does this accomplish?]
 """
+import census
+import us
+
+from census import Census
+from us import states
+
+c = Census("3f2ba2143b0d33ef1ceec3c75c4090499c59c5a6")
+print(c.acs5.state_county(('NAME','B25034_010E'),states.AK.fips, '170'))
+
+
+
+
 ########################### 
 # GLOBAL VARIABLES
 ###########################
